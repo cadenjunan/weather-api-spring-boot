ef48a10b2bcb150200ebf2822414ac776fc97ccf
cadenjunan <cadenjunan@gmail.com>
2026-02-15 12:05:36 +0800

    update request api mapper to call the weather requester internally

--- main/src/main/java/com/weather/api/main/configuration/WeatherAPIConfiguration.java
+++ main/src/main/java/com/weather/api/main/configuration/WeatherAPIConfiguration.java
@@ -1,16 +1,30 @@
 package com.weather.api.main.configuration;
 
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.weather.api.main.service.DateFormatValidator;
+import com.weather.api.main.service.DateParamValidator;
 
 @Configuration
 public class WeatherAPIConfiguration {
 
     @Bean
-    WeatherRequestAPIMapper weatherRequestAPIMapper() {
+    DateParamValidator dateParamValidator() {
+        return DateParamValidator.builder().addValidator(
+                new DateFormatValidator(DateTimeFormatter.ISO_LOCAL_DATE))
+                .addValidator(new DateFormatValidator(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .build();
+    }
+
+    @Bean
+    WeatherRequestAPIMapper weatherRequestAPIMapper(DateParamValidator dateParamValidator) {
         return WeatherRequestAPIMapper.APIMapperBuilder.builder()
                 .add("air-temperature", "https://api-open.data.gov.sg/v2/real-time/api/air-temperature")
                 .add("rainfall", "https://api-open.data.gov.sg/v2/real-time/api/rainfall")
+                .addDateStringValidator(dateParamValidator)
                 .build();
     }
 }
--- main/src/main/java/com/weather/api/main/configuration/WeatherRequestAPIMapper.java
+++ main/src/main/java/com/weather/api/main/configuration/WeatherRequestAPIMapper.java
@@ -3,22 +3,37 @@ package com.weather.api.main.configuration;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.weather.api.main.entities.WeatherAPIResponse;
+import com.weather.api.main.error.InvalidDateFormatException;
+import com.weather.api.main.service.DateParamValidator;
 import com.weather.api.main.weatherrequest.WeatherRequest;
 
 public class WeatherRequestAPIMapper {
     private Map<String, WeatherRequest> requestAPIMapper;
-
-    public WeatherRequestAPIMapper(Map<String, WeatherRequest> requestAPIMapper) {
+    private DateParamValidator dateParamValidator;
+    public WeatherRequestAPIMapper(Map<String, WeatherRequest> requestAPIMapper, DateParamValidator dateParamValidator) {
         this.requestAPIMapper = requestAPIMapper;
+        this.dateParamValidator = dateParamValidator;
     }
 
-    public WeatherRequest getRequester(String name) {
-        return this.requestAPIMapper.get(name);
+    public WeatherAPIResponse getDataReading(String name){
+        return this.requestAPIMapper.get(name).getReadings();
+    }
+    public String getDataReadingText(String name){
+        return this.requestAPIMapper.get(name).getReadingsInText();
+    }
+
+    public WeatherAPIResponse getDataReadingByDate(String name, String dateString){
+        boolean validDateFormat = this.dateParamValidator.validate(dateString);
+        if(!validDateFormat){
+            throw new InvalidDateFormatException();
+        }
+        return this.requestAPIMapper.get(name).getReadindgsWithDate(dateString);
     }
 
     public static class APIMapperBuilder {
         private Map<String, WeatherRequest> requestAPIMapper;
-
+        private DateParamValidator dateParamValidator;
         private APIMapperBuilder() {
             this.requestAPIMapper = new HashMap<>();
         }
@@ -31,6 +46,10 @@ public class WeatherRequestAPIMapper {
             this.requestAPIMapper.put(name, request);
             return this;
         }
+        public APIMapperBuilder addDateStringValidator(DateParamValidator dateParamValidator){
+            this.dateParamValidator = dateParamValidator;
+            return this;
+        }
 
         public APIMapperBuilder add(String name, String uri) {
             this.requestAPIMapper.put(name, new WeatherRequest(uri));
@@ -38,7 +57,7 @@ public class WeatherRequestAPIMapper {
         }
 
         public WeatherRequestAPIMapper build() {
-            return new WeatherRequestAPIMapper(requestAPIMapper);
+            return new WeatherRequestAPIMapper(requestAPIMapper, dateParamValidator);
         }
     }
 
